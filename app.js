4a50a2a55169371059770f6cdaf3a561744b5fbf
James Okolie <zebedee788@gmail.com>
2023-05-06 23:57:22 +0400

    main function and aggregate pipeline created

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
-
-
+// Using the MongoDB Aggregation Stage with Nodejs: $match and $group
 const { MongoClient } = require('mongodb')
 require('dotenv').config()
 
 const uri = process.env.MONGO_URI
-console.log(uri)
-// const client = new MongoClient(uri)
+const client = new MongoClient(uri)
 
-// const connectToDatabase = async () => {
-//     try{
-//         await client.connect()
-//         console.log(`Connected to the ${dbname} database \nFull connection string: ${uri}`)
-//     }catch (err) {
-//         console.error(`Error connecting to the database: ${err}`)
-//     }
-// }
+const connectToDatabase = async () => {
+    try{
+        await client.connect()
+        console.log(`Connected to the ${dbname} database \nFull connection string: ${uri}`)
+    }catch (err) {
+        console.error(`Error connecting to the database: ${err}`)
+    }
+}
 
-// const dbname = 'bank'
-// const collection_name = 'accounts'
-// const accountsCollection = client.db(dbname).collection(collection_name)
+const pipeline = [
+    // Stage 1: match the accounts with a balance greater than $1,000
+    { $match: { balance: { $gt: 1000 } } },
+
+    // Stage 2: calculate average balance and total balance
+    {
+        $group: {
+            _id: '$account_type',
+            total_balance: { $sum: '$balance' },
+            avg_balance: { $avg: '$balance' },
+        },
+    },
+]
+
+const main = async () => {
+    try{
+        await connectToDatabase()
+        let accounts = client.db('bank').collection('accounts')
+        let result = await accounts.aggregate(pipeline)
+        for await (const doc of result) {
+            console.log(doc)
+        }
+    }catch (err) {
+        console.error(`Error connecting to the database: ${err}`)
+    }finally {
+        await client.close()
+    }
+}
+
+main()
